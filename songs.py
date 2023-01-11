8857e8d45b83fbc6b8b3153c4a915d6b39013558
ramalmul <91960510+ramalmul@users.noreply.github.com>
2023-01-11 18:46:23 +0200

    created list and dictionary or artist song
    also deleted unused var

--- songs.py
+++ songs.py
@@ -1,11 +1,9 @@
 from bs4 import BeautifulSoup
 import requests
 
-artistname = ""
-songname = ""
 artistlist =[]
 songlist = []
-dic = {"artist":"","song":""}
+dic = {}
 url = "https://www.ultimate-guitar.com/top/tabs"
 result = requests.get(url)
 doc = BeautifulSoup(result.text, "html.parser")
@@ -15,18 +13,14 @@ artist_nice= str(artist).split("{&quot;")
 for row in artist_nice:
     newline = row[row.find("/tab/"):row.find("&quote")]
     actualsong = newline.split("/")
-
     if len(actualsong) > 3:
         tempsong=actualsong[3].replace("-"," ").split("chord")
         tempsong2 = tempsong[0].split("tabs")
         tempartist = actualsong[2].replace("-"," ")
-        songlist.append(str(tempsong2[0]))
+        songlist.append(str(tempsong2[0].strip()))
         artistlist.append(str(tempartist))
 
 for x in range(len(songlist)):
-    print(artistlist[x]+ " - "+ songlist[x])
-
-
-
-    
-    
+    print(str(x+1)+ " " + artistlist[x]+ " - "+ songlist[x])
+    dic[artistlist[x]] = songlist[x]
+print(dic)
