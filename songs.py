803d4d666cb1ad54c1939f1961f2544c56e0bc61
ramalmul <91960510+ramalmul@users.noreply.github.com>
2023-01-11 18:14:55 +0200

    deleted comments

--- songs.py
+++ songs.py
@@ -11,42 +11,20 @@ result = requests.get(url)
 doc = BeautifulSoup(result.text, "html.parser")
 artist = doc.find_all(class_="js-store")
 artist_nice= str(artist).split("{&quot;")
-#print(artist)
-#print(artist_nice)
-#print(doc.prettify())
 
 for row in artist_nice:
     newline = row[row.find("/tab/"):row.find("&quote")]
-    # print(newline[5:])
     actualsong = newline.split("/")
-    # print(actualsong)
-    # print(type(actualsong))
-    # print(len(actualsong))
+
     if len(actualsong) > 3:
-        # artistname= "Artist: "+str(actualsong[2])
-        # print(artistname)
-        # songname= "Song:" +str(actualsong[3])
-        # print(songname[:songname.find("-chord")])
-        # dic["artist"]=artistname
-        # dic["song"]=songname
         tempsong=actualsong[3].replace("-"," ").split("chord")
         tempsong2 = tempsong[0].split("tabs")
         tempartist = actualsong[2].replace("-"," ")
-        # print(tempsong2)
-        # ch=tr.split("chord")
-        # ch1 = str(tr)
         songlist.append(str(tempsong2[0]))
         artistlist.append(str(tempartist))
 
 for x in range(len(songlist)):
     print(artistlist[x]+ " - "+ songlist[x])
-    
-# for songs in songlist:
-#     tr = songs
-#     tr2 = tr.replace("-"," ")
-#     print(tr2)
-#     ch= tr2.split("chord")
-#     print(ch[0])
 
 
 
